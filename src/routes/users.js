8e3f06d335272cf146132e7f73de2ca6887ef592
ferchuqui <luisfernandofarji@hotmail.com>
2019-10-16 22:15:48 -0300

    Fix errors signin

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,39 @@ const router = require('express').Router();
 const User = require('../models/User');
 const passport = require('passport');
 
+router.get('/users/signup', (req, res) => {
+  res.render('users/signup');
+});
+
+router.post('/users/signup', async(req, res) =>{
+  let errors = [];
+  const { name, email, password, confirm_password} = req.body;
+  
+  if(password != confirm_password) {
+    errors.push({text: 'Passwords do not match.'});
+  }
+  if(password.length < 4) {
+    errors.push({text: 'Passwords must be at least 4 characters.'})
+  }
+  if(errors.length > 0){
+    res.render('users/signup', {errors, name, email, password, confirm_password});
+  } else {
+    // Look for email coincidence
+    const emailUser = await User.findOne({email: email});
+    if(emailUser) {
+      req.flash('error_msg', 'The Email is already in use.');
+      res.redirect('/users/signup');
+    } else {
+      // Saving a New User
+      const newUser = new User({name, email, password});
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save();
+      req.flash('success_msg', 'You are registered.');
+      res.redirect('/users/signin');
+    }
+  }
+});
+
 router.get('/users/signin', async(req, res) => {
   res.render('users/signin');
 });
@@ -11,32 +44,13 @@ router.post('/users/signin', passport.authenticate('local', {
   successRedirect : '/notes',
   failureRedirect: '/users/signin',
   failureFlash: true
-}))
-router.get('/users/signup', (req, res) => {
-  res.render('users/signup');
+}));
+
+router.get('/users/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out now.');
+  res.redirect('/users/signin');
 });
 
-router.post('/users/signup', async(req, res) =>{
-  const { name, email, password, confirm_password} = req.body;
-  const errors = [];
-  if (name.length<=0) {
-    errors.push({text: 'Please insert your name'})
-  }
-  if (password != confirm_password) {
-    errors.push({text:'Password does not match'})
-  }
-  if (password.length<4) {
-    errors.push({text: 'Password length must be at least 4 characters'})
-  }
-  if (errors.lengt>0) {
-    res.render('/users/signup', {errors, name, email, password, confirm_password})
-  } else {
-    const newUser = new User({name, email, password});
-    newUser.password = await newUser.encryptPassword(password);
-    await newUser.save();
-    req.flash('success_msg', 'You are registered')
-    res.redirect('/users/signin')
-  }
 
-});
 module.exports = router;
